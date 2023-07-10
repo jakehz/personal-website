8cbcfbb0b7ac94814ef93690b439e34867de1ddd
Jake Hernandez <jakejhdz@gmail.com>
2023-07-09 20:16:06 -0700

    Add link to resume

--- src/Main.js
+++ src/Main.js
@@ -2,11 +2,14 @@
 function Main() {
     const style = {
         "zIndex": 2,
-        "color": 'white' 
+        "color": 'white',
+        "a:link": 'white',
+        "a:unvisited": 'white'
     }
-    return (<div style={style}>
-        <h1 class='center'>Hi there</h1>
-        <h2 class='center'>My name is Jake Hernandez, I'm a software engineer.</h2>
+    return (<div style={style} class="center">
+        <h1>Hi there</h1>
+        <h2>My name is Jake Hernandez, I'm a software engineer.</h2>
+        <p>You can view my resume <a style={style} href="https://docs.google.com/document/d/e/2PACX-1vQAErMMCmruRVT9GDU3Nkc28MzZ_2i0xge4jRktkOZ9DVrwuZ--09PHVBNs6WwnzDeke0Zur60zsfyZ/pub">here.</a></p>
     </div>
     )
 }
