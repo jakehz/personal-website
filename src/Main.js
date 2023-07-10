af22a6ca54add9fab22c511f1c0b195cf8d73913
Jake Hernandez <jakejhdz@gmail.com>
2023-07-09 20:20:13 -0700

    Change main page text

--- src/Main.js
+++ src/Main.js
@@ -8,7 +8,7 @@ function Main() {
     }
     return (<div style={style} class="center">
         <h1>Hi there</h1>
-        <h2>My name is Jake Hernandez, I'm a software engineer.</h2>
+        <h2>My name is Jacob Hernandez, I'm a software engineer.</h2>
         <p>You can view my resume <a style={style} href="https://docs.google.com/document/d/e/2PACX-1vQAErMMCmruRVT9GDU3Nkc28MzZ_2i0xge4jRktkOZ9DVrwuZ--09PHVBNs6WwnzDeke0Zur60zsfyZ/pub">here.</a></p>
     </div>
     )
