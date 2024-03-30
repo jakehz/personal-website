e33fafd83c183c759dde78d8f77bb7d095d9b262
Jake Hernandez <jakejhdz@gmail.com>
2024-03-30 11:43:27 -0700

    Change link to resume

--- src/Main.js
+++ src/Main.js
@@ -1,17 +1,25 @@
-
 function Main() {
-    const style = {
-        "zIndex": 2,
-        "color": 'white',
-        "a:link": 'white',
-        "a:unvisited": 'white'
-    }
-    return (<div style={style} class="center">
-        <h1>Hi there</h1>
-        <h2>My name is Jacob Hernandez, I'm a software engineer.</h2>
-        <p>You can view my resume <a style={style} href="https://docs.google.com/document/d/e/2PACX-1vQAErMMCmruRVT9GDU3Nkc28MzZ_2i0xge4jRktkOZ9DVrwuZ--09PHVBNs6WwnzDeke0Zur60zsfyZ/pub">here.</a></p>
+  const style = {
+    zIndex: 2,
+    color: "white",
+    "a:link": "white",
+    "a:unvisited": "white",
+  };
+  return (
+    <div style={style} class="center">
+      <h1>Hi there</h1>
+      <h2>My name is Jacob Hernandez, I'm a software engineer.</h2>
+      <p>
+        You can view my resume{" "}
+        <a
+          style={style}
+          href="https://drive.google.com/drive/folders/1b26qinS5cujA71rPf8Asql1_SivXRxiG?usp=drive_link"
+        >
+          here.
+        </a>
+      </p>
     </div>
-    )
+  );
 }
 
-export default Main
+export default Main;
